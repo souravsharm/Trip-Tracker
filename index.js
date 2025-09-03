e39e1b14af4ad926ac95668b07daa6dfce050450
souravsharma <msg2souravsharma@gmail.com>
2025-09-03 18:56:32 +1000

    Trip Tracker join table added

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function findColor(id){
   }
 }
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries WHERE user_id = $1",[currentUserId]);
+  const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1",[currentUserId]);
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code.trim());
